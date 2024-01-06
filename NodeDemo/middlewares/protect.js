94046bdfe15f474db32797e60eb11d89b11a07b9
TUNGNT <nguyenthanhtunnghutechsg@gmail.com>
2024-01-06 09:57:17 +0700

    get token from cookie, logout, forgot password

--- NodeDemo/middlewares/protect.js
+++ NodeDemo/middlewares/protect.js
@@ -6,18 +6,20 @@ module.exports = {
         async function (req) {
             var result = {}
             var token = req.headers.authorization;
-            if (!token) {
-                return result.err = "Vui long dang nhap";
-            }
-            if (token.startsWith("Bearer")) {
-                token = token.split(" ")[1];
-                try {
-                    var userID = await jwt.verify(token, configs.SECRET_KEY);
-                    return userID.id;
-                } catch (error) {
-                    return result.err = "Vui long dang nhap";
-                }
+            
+            if (token&&token.startsWith("Bearer")) {
+                token = token.split(" ")[1];             
             } else {
+                if(req.cookies.tokenJWT){
+                    token = req.cookies.tokenJWT;
+                }else{
+                    return result.err = "Vui long dang nhap";
+                }             
+            }
+            try {
+                var userID = await jwt.verify(token, configs.SECRET_KEY);
+                return userID.id;
+            } catch (error) {
                 return result.err = "Vui long dang nhap";
             }
         },
--- NodeDemo/schema/user.js
+++ NodeDemo/schema/user.js
@@ -1,19 +1,25 @@
 var mongoose = require("mongoose");
 const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken')
+const jwt = require('jsonwebtoken');
+const crypto = require('crypto');
 const configs = require('../helper/configs')
 
 const schema = new mongoose.Schema({
     email: String,
     userName: String,
     password: String,
-    role: String
+    role: String,
+    tokenForgot:String,
+    tokenForgotExp:String
 });
 
-schema.pre('save', function () {
+schema.pre('save', function (next) {
+    if(!this.isModified("password")){
+        return next();
+    }
     const salt = bcrypt.genSaltSync(10);
     this.password = bcrypt.hashSync(this.password, salt);
-    //bug sinh ra khi change password
+    next();
 })
 
 schema.methods.getJWT = function () {
@@ -21,6 +27,14 @@ schema.methods.getJWT = function () {
         { expiresIn: configs.EXP });
     return token;
 }
+
+schema.methods.addTokenForgotPassword= function(){
+    var tokenForgot = crypto.randomBytes(31).toString('hex');
+    this.tokenForgot = tokenForgot;
+    this.tokenForgotExp = Date.now()+15*60*1000;
+    return tokenForgot;
+}
+
 schema.statics.checkLogin = async function (userName, password) {
     if (!userName || !password) {
         return { err: 'Hay nhap day du username va password' };
